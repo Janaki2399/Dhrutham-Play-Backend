33f7947fd4a190e69c49e15b6226ffef4f8af347
janaki2399 <janakishreeja23@gmail.com>
2021-05-28 10:55:39 -0700

    feat - create playlist

--- routes/library-router.js
+++ routes/library-router.js
@@ -41,86 +41,94 @@ router.route("/").get(async (req, res) => {
     });
   }
 });
-// router.post("/:playlistId", async (req, res) => {
-//   try {
-//     const video = req.body;
-//     const id = req.params.playlistId;
-//     let playlist = await Playlist.findById(id);
-//     playlist = _.extend(playlist, {
-//       playlist: _.concat(Playlist.playlist, video._id),
-//     });
-//     const updated = await playlist.save();
-//     const populated = await updated
-//       .populate({ path: "playlist", populate: { path: "playlist" } })
-//       .execPopulate();
-//     res.status(200).json({ updated: populated });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       errorMessage: error.message,
-//     });
-//   }
-// });
-router
-  .route("/:playlistId")
-  .get(async (req, res) => {
-    const playlistId = req.params.playlistId;
-    // console.log(id);
-    const playlist = await Playlist.findById(playlistId).populate();
-
-    res.json({ playlist, success: true });
-  })
-  .post(async (req, res) => {
-    try {
-      //create playlist if not created -TODO
-      const video = req.body;
-      const id = req.params.playlistId;
-      let playlist = await Playlist.findById(id);
-      playlist = _.extend(playlist, {
-        playlist: _.concat(playlist.playlist, video._id),
-      });
-
-      const updated = await playlist.save();
-      const populated = await updated
-        .populate({ path: "playlist", populate: { path: "playlist" } })
-        .execPopulate();
-      res.status(200).json({ updated: populated });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        errorMessage: error.message,
-      });
-    }
-  })
-  .delete(async (req, res) => {
-    const playlistId = req.params.playlistId;
-
-    const playlistItem = await Playlist.findById(playlistId); //check this out
-    await playlistItem.remove();
-    res.status(200).json({ success: true });
-  });
-
-router
-  .route("/:playlistId/:videoId")
-  .get(async (req, res) => {
-    const playlistId = req.params.playlistId;
-    const videoId = req.params.videoId;
-
-    const playlist = await Playlist.findById(playlistId);
-    const populatedPlaylist = await playlist
-      .populate("playlist")
-      .execPopulate();
-    const video = await Video.findById(videoId);
-
-    res.status(200).json({ playlist: populatedPlaylist, video, success: true });
-  })
-  .delete(async (req, res) => {
-    const playlistId = req.params.playlistId;
-    const videoId = req.params.videoId;
-
-    let playlist = await Playlist.findById(playlistId);
-    playlist.playlist.pull({ _id: videoId });
+router.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+    const { userId } = req.user;
+    const playlist = new Playlist(body);
     await playlist.save();
+    const library = new Library({
+      userId,
+      list: { _id: playlist._id },
+    });
+    await library.save();
+    // const id = req.params.playlistId;
+    // let playlist = await Playlist.findById(id);
+    // playlist = _.extend(playlist, {
+    //   playlist: _.concat(Playlist.playlist, video._id),
+    // });
+    // const updated = await playlist.save();
+    // const populated = await updated
+    //   .populate({ path: "playlist", populate: { path: "playlist" } })
+    //   .execPopulate();
     res.status(200).json({ success: true });
-  });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      errorMessage: error.message,
+    });
+  }
+});
+// router
+//   .route("/:playlistId")
+//   .get(async (req, res) => {
+//     const playlistId = req.params.playlistId;
+//     // console.log(id);
+//     const playlist = await Playlist.findById(playlistId).populate();
+
+//     res.json({ playlist, success: true });
+//   })
+//   .post(async (req, res) => {
+//     try {
+//       //create playlist if not created -TODO
+//       const video = req.body;
+//       const id = req.params.playlistId;
+//       let playlist = await Playlist.findById(id);
+//       playlist = _.extend(playlist, {
+//         playlist: _.concat(playlist.playlist, video._id),
+//       });
+
+//       const updated = await playlist.save();
+//       const populated = await updated
+//         .populate({ path: "playlist", populate: { path: "playlist" } })
+//         .execPopulate();
+//       res.status(200).json({ updated: populated });
+//     } catch (error) {
+//       res.status(500).json({
+//         success: false,
+//         errorMessage: error.message,
+//       });
+//     }
+//   })
+//   .delete(async (req, res) => {
+//     const playlistId = req.params.playlistId;
+
+//     const playlistItem = await Playlist.findById(playlistId); //check this out
+//     await playlistItem.remove();
+//     res.status(200).json({ success: true });
+//   });
+
+// router
+//   .route("/:playlistId/:videoId")
+//   .get(async (req, res) => {
+//     const playlistId = req.params.playlistId;
+//     const videoId = req.params.videoId;
+
+//     const playlist = await Playlist.findById(playlistId);
+//     const populatedPlaylist = await playlist
+//       .populate("playlist")
+//       .execPopulate();
+//     const video = await Video.findById(videoId);
+
+//     res.status(200).json({ playlist: populatedPlaylist, video, success: true });
+//   })
+//   .delete(async (req, res) => {
+//     const playlistId = req.params.playlistId;
+//     const videoId = req.params.videoId;
+
+//     let playlist = await Playlist.findById(playlistId);
+//     playlist.playlist.pull({ _id: videoId });
+//     await playlist.save();
+//     res.status(200).json({ success: true });
+//   });
 module.exports = router;
